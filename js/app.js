bcad326cc6018d9dd81b0b0fbafe9080a7e0aef1
kou-rose <chelsearola@gmail.com>
2022-08-17 21:35:16 -0400

    code to filter table

--- js/app.js
+++ js/app.js
@@ -22,3 +22,26 @@ function buildTable(data) {
         cell.text(val);
         });}
     );}
+
+function handleClick() {
+    // get datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // check to see if date was entered and
+    // filter the data using that date
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // rebuild the table using the filtered data
+    // NOTE: if no date was entered, then filteredData
+    // will just be the original tableData
+    buildTable(filteredData);
+
+    // attach an event to listen for the form button
+    d3.selectAll("#filter-btn").on("click", handleClick);
+
+    // build the table when the page loads
+    buildTable(tableData);
+}
